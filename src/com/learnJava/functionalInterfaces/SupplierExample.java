d15a874a871d3b043987a0ce2439ac4ff3514ffb
Sumit <mailmeatsumitsingh@gmail.com>
2024-11-18 00:27:11 +0530

    Explanation of BinaryOperator, UnaryOperator, supplier

---
+++ src/com/learnJava/functionalInterfaces/SupplierExample.java
@@ -0,0 +1,30 @@
+package com.learnJava.functionalInterfaces;
+
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierExample {
+
+    /*
+    * functional interface with single abstract method supply which doesn't take any input
+    * but returns an output
+    * */
+
+    public static void main(String[] args) {
+        Supplier<Student> supplierExample=()->{
+return new Student("Adam",2,3.6, "male",10, Arrays.asList("swimming", "basketball","volleyball"));
+        };
+
+        Supplier<List<Student>> allStudents=()->{
+            return StudentDataBase.getAllStudents();
+        };
+
+        System.out.println("student is: "+supplierExample.get());
+
+        System.out.println("All students: "+allStudents.get());
+    }
+}
