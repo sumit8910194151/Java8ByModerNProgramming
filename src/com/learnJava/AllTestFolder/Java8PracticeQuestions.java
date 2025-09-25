edde8097a45d4c4d64b2bbd5bf1a548ae143beb8
Sk1997 <sksingh@gairik.in>
2025-09-25 09:24:00 +0530

    Added java8 practice session for 25th september

--- src/com/learnJava/AllTestFolder/Java8PracticeQuestions.java
+++ src/com/learnJava/AllTestFolder/Java8PracticeQuestions.java
@@ -1,6 +1,8 @@
 package com.learnJava.AllTestFolder;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -32,5 +34,26 @@ public class Java8PracticeQuestions {
 
 
         System.out.println("odd of array: "+list1.stream().filter(e->e%2!=0).collect(Collectors.toSet()) );
+
+        System.out.println("Start number with 1: "+list1.stream().filter(e->String.valueOf(e).startsWith("1")).collect(Collectors.toSet()));
+
+        System.out.println("max number in array: "+list1.stream().reduce(Integer :: max));
+        System.out.println("min number in array: "+list1.stream().reduce(Integer :: min));
+        System.out.println("get second largest number: "+ list1.stream().sorted(Comparator.reverseOrder()).limit(2).skip(1).findFirst());
+        System.out.println("get largest two number: "+ list1.stream().sorted(Comparator.reverseOrder()).limit(2).collect(Collectors.toSet()));
+        System.out.println("get duplicates: "+list1.stream().filter(e-> Collections.frequency(list1,e)>1).collect(Collectors.toSet()));
+        System.out.println("get non duplicates: "+list1.stream().filter(e-> Collections.frequency(list1,e)==1).collect(Collectors.toSet()));
+        List<Integer> list2=Arrays.asList(11,21,34,21,31,32);
+        List<Integer> list3=Arrays.asList(11,21,22,32,43,33);
+        System.out.println("common element in both the lists: "+ list3.stream().filter(e->list2.contains(e)).collect(Collectors.toSet()));
+        String str="Ie am the best";
+        List<String> strList=Arrays.asList(str.split(""));
+        System.out.println(strList);
+        System.out.println("find first repeated character: "+ strList.stream().filter(e->Collections.frequency(strList,e)>1).findFirst());
+        System.out.println("non repeated chars: "+ strList.stream().filter(e->Collections.frequency(strList,e)==1).collect(Collectors.toSet()));
+        System.out.println("Capitalize e: "+ strList.stream().map(e->e.equals("e")?e.toUpperCase():e).collect(Collectors.joining()));
+        System.out.println("Reverse string: "+strList.stream().reduce((a,b)->b+a));
+        System.out.println("Reverse word by word: "+Arrays.asList(str.split(" ")).stream().reduce((a,b)->b+" " +a));
+
     }
 }
